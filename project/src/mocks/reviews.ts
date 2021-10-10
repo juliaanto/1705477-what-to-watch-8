939e5bb362b6f8371a0ea38de030f5cad3ac62e3
juliaanto <1306577@mail.ru>
2021-10-10 19:34:12 +0300

    Добавлены моки фильмов и отзывов

---
+++ project/src/mocks/reviews.ts
@@ -0,0 +1,24 @@
+import {Review} from '../types/review';
+
+export const reviews: Review[] = [
+  {
+    id: 1,
+    user: {
+      id: 4,
+      name: 'Kate Muir',
+    },
+    rating: 8.9,
+    comment: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director\'s funniest and most exquisitely designed movies in years.',
+    date: '2019-05-08T14:13:56.569Z',
+  },
+  {
+    id: 2,
+    user: {
+      id: 5,
+      name: 'Matthew Lickona',
+    },
+    rating: 7.2,
+    comment: 'The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.',
+    date: '2019-03-24T13:43:03.569Z',
+  },
+];
