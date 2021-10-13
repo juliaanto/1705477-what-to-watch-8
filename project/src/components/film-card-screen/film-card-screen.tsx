e6735d99c69acdce8c43ecc7dd65251ae4cd51bb
juliaanto <1306577@mail.ru>
2021-10-13 22:23:53 +0300

    Добавлена активная карточка фильма в стейт компонента

--- project/src/components/film-card-screen/film-card-screen.tsx
+++ project/src/components/film-card-screen/film-card-screen.tsx
@@ -9,16 +9,15 @@ function FilmCardScreen(props: FilmCardScreenProps): JSX.Element {
   const {film} = props;
   const {name, previewImage} = film;
 
-
   return (
-    <article className="small-film-card catalog__films-card">
+    <>
       <div className="small-film-card__image">
         <img src={previewImage} alt={name} width="280" height="175" />
       </div>
       <h3 className="small-film-card__title">
         <a className="small-film-card__link" href="film-page.html">{name}</a>
       </h3>
-    </article>
+    </>
   );
 }
 
--- project/src/components/film-list/film-list.tsx
+++ project/src/components/film-list/film-list.tsx
@@ -1,3 +1,4 @@
+import {useState, MouseEvent} from 'react';
 import {Films} from '../../types/film';
 import FilmCardScreen from '../film-card-screen/film-card-screen';
 
@@ -8,13 +9,26 @@ type FilmListProps = {
 function FilmList(props: FilmListProps): JSX.Element {
   const {films} = props;
 
+  const [, setActiveCard] = useState({});
+
   return (
     <div className="catalog__films-list">
       {films.map((film, id) => {
         const keyValue = `${id}`;
 
         return (
-          <FilmCardScreen key={keyValue} film={film} />
+          <article key={keyValue} className="small-film-card catalog__films-card"
+            onMouseEnter={({target}: MouseEvent<HTMLElement>) => {
+              setActiveCard(film);
+            }}
+            onMouseLeave={({target}: MouseEvent<HTMLElement>) => {
+              setActiveCard([{}]);
+            }}
+          >
+            <FilmCardScreen
+              film={film}
+            />
+          </article>
         );
       })};
     </div>
