c191ef2796f15c744532acf7ecffa41aeff73062
juliaanto <1306577@mail.ru>
2021-10-29 18:21:31 +0300

    Добавлена функция получения фильмов определенного жанра

---
+++ project/src/utils/films.ts
@@ -0,0 +1,12 @@
+import { ALL_GENRES } from '../const';
+import {films} from '../mocks/films';
+import {Films} from '../types/film';
+
+export const getFilmsBygenre = (genre: string): Films => {
+
+  if (genre === ALL_GENRES) {
+    return films;
+  }
+
+  return films.filter((film) => film.genre === genre);
+};
