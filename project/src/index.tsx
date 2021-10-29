9913929debc0ca8935745fad2bff746c33e74b6a
juliaanto <1306577@mail.ru>
2021-10-29 18:03:59 +0300

    Добавлена поддержка Redux DevTools

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -5,8 +5,12 @@ import {createStore} from 'redux';
 import App from './components/app/app';
 import {films, PROMO} from './mocks/films';
 import {reducer} from './store/reducer';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  composeWithDevTools(),
+);
 
 ReactDOM.render(
   <React.StrictMode>
