237886ad2f723f43b8021b4f6b8ebe29c3d0f881
juliaanto <1306577@mail.ru>
2021-10-31 19:32:59 +0300

    review item вынесен в отдельный компонент

---
+++ project/src/components/review-item.tsx/review-item.tsx
@@ -0,0 +1,27 @@
+import dayjs from 'dayjs';
+import {Review} from '../../types/review';
+
+type ReviewItemProps = {
+  review: Review;
+}
+
+function ReviewItem(props: ReviewItemProps): JSX.Element {
+  const {review} = props;
+
+  return (
+    <div key={review.id} className="review">
+      <blockquote className="review__quote">
+        <p className="review__text">{review.comment}</p>
+
+        <footer className="review__details">
+          <cite className="review__author">{review.user.name}</cite>
+          <time className="review__date" dateTime={dayjs(review.date).format('YYYY-MM-DD')}>{dayjs(review.date).format('MMMM DD, YYYY')}</time>
+        </footer>
+      </blockquote>
+
+      <div className="review__rating">{review.rating.toLocaleString()}</div>
+    </div>
+  );
+}
+
+export default ReviewItem;
